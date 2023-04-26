83e3632fd35aef6237b1329c7ac4156042f42abc
Lumano <35842223+LuizGermano@users.noreply.github.com>
2023-04-26 15:19:51 -0300

    Update Principal.java

--- Jogo/Principal.java
+++ Jogo/Principal.java
@@ -21,7 +21,7 @@ public class Principal {
         
         System.out.println("O guerreiro "+jogador.nome + " atacou o guerreiro " +jogador2.getNome());
         jogador.ataca(jogador2);
-        System.out.println(jogador2.nome+ " perdeu 5 pontos de energia.");
+        System.out.println(jogador2.nome+ " perdeu pontos de energia.");
         System.out.println("\n");
         System.out.println("O guerreiro "+jogador2.nome + " atacou o guerreiro " +jogador.getNome());
         jogador2.ataca(jogador);
@@ -30,11 +30,12 @@ public class Principal {
         System.out.println(jogador.nome+ " você recebeu uma espada e sua armadura.");
         System.out.println("O guerreiro "+jogador.nome + " atacou o guerreiro " +jogador2.getNome() + " com a "+jogador.arma);
         jogador.atacar(jogador2);
+        System.out.println(jogador2.nome+ " perdeu pontos de energia.");
         System.out.println("\n");
         System.out.println(jogador2.nome+ " você não recebeu uma arma.");
         System.out.println("O guerreiro "+ jogador2.nome + " atacou o guerreiro " +jogador.getNome());
-        jogador2.ataca(jogador);
-        System.out.println(jogador.nome+ " perdeu 5 pontos de energia.");
+        jogador2.ataque(jogador);
+        System.out.println(jogador.nome+ " perdeu pontos de energia.");
         
         
         System.out.println(jogador2.nome+ " você tem de energia: " +jogador2.energia);
